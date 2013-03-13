012a0b3fe0bffbe5ea854412523d7c41efc6ab26
Peter Merikan <peter@merikan.com>
2013-03-13 13:07:15 +0100

    fixed the error: change from absent to present failed

--- puppet/modules/java/manifests/install.pp
+++ puppet/modules/java/manifests/install.pp
@@ -3,8 +3,8 @@ class java::install {
   package { $java::params::package_name:
     provider => rpm,
     ensure => installed,
-    source => "/vagrant/puppet/files/${java::params::package_name}"
-    #source => "puppet:///files/${java::params::package_name}"
-
+    source => "/vagrant/puppet/files/${java::params::dist_name}"
+    #source => "puppet:///files/${java::params::dist_name}"
   } 
+
 }
