251f48a10c506679632f75e7cbca8bd4a1bf3986
Peter Merikan <peter@merikan.com>
2013-03-13 08:23:57 +0100

    added new manifests for activemq

--- puppet/modules/tomcat/manifests/init.pp
+++ puppet/modules/tomcat/manifests/init.pp
@@ -3,4 +3,22 @@ class tomcat {
   include tomcat::params, tomcat::install, tomcat::config, tomcat::service
 
   require  java
+
+
+  # skall detta ligga i en egen manifestfil?
+  define tomcat::deployment($path) {
+ 
+    include tomcat
+    notice("Establishing http://$hostname:${tomcat::tomcat_port}/$name/")
+ 
+    file { "${tomcat::params::install_dir}/webapps/${name}.war":
+      owner => 'root',
+      source => $path,
+    }
+  }
+
+  # use tomcat::deploy
+  #tomcat::deployment { "SimpleServlet":
+  #  path => '/srv/puppet-tomcat-demo/java_src/SimpleServlet.war'
+  #}
 }
