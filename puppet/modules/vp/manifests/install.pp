5c14775ad9d6a95cb98ba372b63c317f216ed0d4
Peter Merikan <peter@merikan.com>
2013-05-24 14:47:31 +0200

    added ehr-patientconsent-checkconsent-testproducer. Closes #10

--- puppet/modules/vp/manifests/install.pp
+++ puppet/modules/vp/manifests/install.pp
@@ -28,7 +28,7 @@ class vp::install {
   }
   # extract vp-services
   exec { "vp-services" : 
-    command => "/usr/bin/unzip /vagrant/puppet/files/vp-services-${vp::params::version}.zip",
+    command => "/usr/bin/unzip -u -o /vagrant/puppet/files/vp-services-${vp::params::version}.zip",
     cwd => "${mule::params::mule_home}/apps/vp-services-${vp::params::version}",
     group  => "mule",
     require => File["${mule::params::mule_home}/apps/vp-services-${vp::params::version}"]
@@ -44,12 +44,19 @@ class vp::install {
   
   # unpack virtual services
   exec { "vp:unpack-vp-services-master" : 
-    command => "/usr/bin/unzip /vagrant/puppet/files/vp-services-master.zip -d ${mule::params::mule_home}/apps/vp-services-${vp::params::version}/lib",
+    command => "/usr/bin/unzip -u -o /vagrant/puppet/files/vp-services-master.zip -d ${mule::params::mule_home}/apps/vp-services-${vp::params::version}/lib",
     group  => "mule",
     ##creates => "${mule::params::mule_home}/apps/vp-services-${vp::params::version}/lib/clinicalprocess-healthcond-actoutcome-GetDeliveryMedicalHistory-virtualisering-2.0-RC2.jar",
     require => Exec['vp-services'],
   }
 
-
+  # test stub for samtycke
+  file {"${mule::params::mule_home}/apps/vp-services-${vp::params::version}/lib/ehr-patientconsent-checkconsent-testproducer-1.0.0-SNAPSHOT.jar":
+    source => "/vagrant/puppet/files/ehr-patientconsent-checkconsent-testproducer-1.0.0-SNAPSHOT.jar",
+    owner  => "mule",
+    group  => "mule",
+    mode   => 644,
+    require => Exec['vp-services'],
+  }
 
 }
