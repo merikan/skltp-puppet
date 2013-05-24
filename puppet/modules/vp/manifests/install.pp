121bdeb3d811a6cc876c3d0c78cdf0a683c777d2
Peter Merikan <peter@merikan.com>
2013-05-24 14:18:25 +0200

    Added virtual services tidbokning, samtycke etc. Closes #8

--- puppet/modules/vp/manifests/install.pp
+++ puppet/modules/vp/manifests/install.pp
@@ -1,5 +1,7 @@
 class vp::install {
   require vp::params
+  require mule
+  include mule::params
 
   file {"${mule::params::install_dir}/certs":
     source => "/vagrant/puppet/modules/vp/files/certs/",
@@ -17,10 +19,37 @@ class vp::install {
     mode   => 755,
   }
 
-  file {"${mule::params::mule_home}/apps/vp-services-${vp::params::version}.zip":
-    source => "/vagrant/puppet/files/vp-services-${vp::params::version}.zip",
+  # vp-services directory
+  file {"${mule::params::mule_home}/apps/vp-services-${vp::params::version}":
+    ensure => "directory",
     owner  => "mule",
     group  => "mule",
     mode   => 755,
   }
+  # extract vp-services
+  exec { "vp-services" : 
+    command => "/usr/bin/unzip /vagrant/puppet/files/vp-services-${vp::params::version}.zip",
+    cwd => "${mule::params::mule_home}/apps/vp-services-${vp::params::version}",
+    group  => "mule",
+    require => File["${mule::params::mule_home}/apps/vp-services-${vp::params::version}"]
+  }
+
+#  file {"vp-services":
+#    path   => "${mule::params::mule_home}/apps/vp-services-${vp::params::version}.zip",
+#    source => "/vagrant/puppet/files/vp-services-${vp::params::version}.zip",
+#    owner  => "mule",
+#    group  => "mule",
+#    mode   => 755,
+#  }
+  
+  # unpack virtual services
+  exec { "vp:unpack-vp-services-master" : 
+    command => "/usr/bin/unzip /vagrant/puppet/files/vp-services-master.zip -d ${mule::params::mule_home}/apps/vp-services-${vp::params::version}/lib",
+    group  => "mule",
+    ##creates => "${mule::params::mule_home}/apps/vp-services-${vp::params::version}/lib/clinicalprocess-healthcond-actoutcome-GetDeliveryMedicalHistory-virtualisering-2.0-RC2.jar",
+    require => Exec['vp-services'],
+  }
+
+
+
 }
