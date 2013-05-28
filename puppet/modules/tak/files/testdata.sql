caec4c4d39666eac492fa07f9d76817d0f2c754b
Magnus Larsson <magnus.larsson@callistaenteprise.se>
2013-05-28 19:12:56 +0200

    Updated 2.0-->2.1 on inex.html + added ping-service in tak-sql-script

--- puppet/modules/tak/files/testdata.sql
+++ puppet/modules/tak/files/testdata.sql
@@ -1,26 +1,33 @@
 
 
 INSERT INTO `LogiskAdressat` (`id`, `beskrivning`, `hsaId`, `version`) VALUES
-(1, 'Demo adressat tidbok, vårdcentralen kusten, Kärna', 'HSA-VKK123', 0),
-(2, 'Demo adressat tidbok, vårdcentralen kusten, Marstrand', 'HSA-VKM345', 0),
-(3, 'Demo adressat tidbok, vårdcentralen kusten, Ytterby', 'HSA-VKY567', 0);
+(1, 'Demo adressat tidbok, vardcentralen kusten, Karna', 'HSA-VKK123', 0),
+(2, 'Demo adressat tidbok, vardcentralen kusten, Marstrand', 'HSA-VKM345', 0),
+(3, 'Demo adressat tidbok, vardcentralen kusten, Ytterby', 'HSA-VKY567', 0),
+(4, 'VP''s egna ping-tjanst', 'Ping', 0);
 
 INSERT INTO `Tjanstekontrakt` (`id`, `beskrivning`, `namnrymd`, `version`) VALUES
-(1, 'Tidbokning - GetSubjectOfCareSchedule', 'urn:riv:crm:scheduling:GetSubjectOfCareSchedule:1:rivtabp21', 0);
+(1, 'Tidbokning - GetSubjectOfCareSchedule', 'urn:riv:crm:scheduling:GetSubjectOfCareSchedule:1:rivtabp21', 0),
+(2, 'Ping', 'urn:riv:itinfra:tp:Ping:1:rivtabp20', 0);
 
 INSERT INTO `Tjanstekomponent` (`id`, `adress`, `beskrivning`, `hsaId`, `version`) VALUES
 (1, 'http://33.33.33.33:8080/Schedulr-0.1/ws/GetSubjectOfCareSchedule/1', 'Demo tidbok', 'Schedulr', 0),
-(4, '', 'tp test client', 'tp', 0);
+(4, '', 'tp test client', 'tp', 0),
+(5, 'http://localhost:10000/test/Ping_Service', 'VP intern ping tjänst', 'Ping-service', 0);
 
 INSERT INTO `LogiskAdress` (`id`, `fromTidpunkt`, `tomTidpunkt`, `version`, `logiskAdressat_id`, `rivVersion_id`, `tjanstekontrakt_id`, `tjansteproducent_id`) VALUES
 (1, '2013-05-24', '2113-05-24', 0, 1, 2, 1, 1),
 (2, '2013-05-24', '2113-05-24', 0, 2, 2, 1, 1),
-(3, '2013-05-24', '2113-05-24', 0, 3, 2, 1, 1);
+(3, '2013-05-24', '2113-05-24', 0, 3, 2, 1, 1),
+(4, '2013-05-28', '2113-05-28', 0, 4, 1, 2, 5);
+
 
 INSERT INTO `Anropsbehorighet` (`id`, `fromTidpunkt`, `integrationsavtal`, `tomTidpunkt`, `version`, `logiskAdressat_id`, `tjanstekonsument_id`, `tjanstekontrakt_id`) VALUES
 (1, '2013-05-24', 'I1', '2113-05-24', 0, 1, 4, 1),
 (2, '2013-05-24', 'I1', '2113-05-24', 0, 2, 4, 1),
-(3, '2013-05-24', 'I1', '2113-05-24', 0, 3, 4, 1);
+(3, '2013-05-24', 'I1', '2113-05-24', 0, 3, 4, 1),
+(4, '2013-05-28', 'I2', '2113-05-28', 0, 4, 4, 2);
+
 
 --
 -- Constraints for table `Anropsbehorighet`
