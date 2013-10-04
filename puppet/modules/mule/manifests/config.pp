f7fa34aacdd157f59959ee0aafe51f3988b73505
Magnus Larsson <magnus.larsson@callistaenteprise.se>
2013-10-04 17:30:49 +0200

    Corrected bug in setup of mule config files so that both mule and wrapper.config files are copied to the correct places, activates mule app start order

--- puppet/modules/mule/manifests/config.pp
+++ puppet/modules/mule/manifests/config.pp
@@ -11,11 +11,19 @@ class mule::config {
     mode   => 755
   }
 
+  file {"mule:mule":
+    path => "${mule::params::install_dir}/bin/mule",
+    ensure  => file,
+    source => "/vagrant/puppet/modules/mule/files/mule",
+    owner => mule,
+    group => mule,
+    notify  => Service["${mule::params::service_name}"],
+  } ->
   file {"mule:service-file":
     path => "/etc/init.d/${mule::params::service_name}",
     target => "${mule::params::install_dir}/bin/mule",
     ensure => link,
-  }
+  } ->
   replace { "mule:replace_chkconfig":
     file => "${mule::params::install_dir}/bin/mule",
     pattern => "# chkconfig: 2345 20 80",
@@ -27,7 +35,7 @@ class mule::config {
     replacement => "RUN_AS_USER=mule"
   } ->
   file {"mule:wrapper.conf":
-    path => "${mule::params::install_dir}/wrapper.conf",
+    path => "${mule::params::install_dir}/conf/wrapper.conf",
     ensure  => file,
     source => "/vagrant/puppet/modules/mule/files/wrapper.conf",
     owner => mule,
