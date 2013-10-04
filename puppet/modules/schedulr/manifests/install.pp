dd7801e288e7571cbb61c458980597c841c7df60
Magnus Larsson <magnus.larsson@callistaenteprise.se>
2013-10-04 13:40:18 +0200

    Added missing config file for Schedulr (copy Schedulr-config.groovy to /root/.grails)

--- puppet/modules/schedulr/manifests/install.pp
+++ puppet/modules/schedulr/manifests/install.pp
@@ -1,6 +1,19 @@
 class schedulr::install {
   require tomcat
 
+  file {"/root/.grails":
+    ensure => directory,
+    recurse => false,
+    owner => root,
+    group => root,
+  } ->
+
+  file {"/root/.grails/Schedulr-config.groovy":
+    source => "/vagrant/puppet/modules/schedulr/files/Schedulr-config.groovy",
+    owner  => "root",
+    group  => "root",
+  } ->
+
   file {"${tomcat::params::tomcat_home}/webapps/${schedulr::params::distname}":
     source => "/vagrant/puppet/files/${schedulr::params::distname}",
     ensure => file,
