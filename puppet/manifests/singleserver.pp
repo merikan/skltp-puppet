bddce7a53736b20c1f518ced2bb2b315af94e76b
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2013-11-30 22:10:17 +0100

    Shortcuts on the desktop and in the panel

--- puppet/manifests/singleserver.pp
+++ puppet/manifests/singleserver.pp
@@ -16,25 +16,60 @@ class singleserver {
     managehome => true,
     gid => "skltp",
     shell => "/bin/bash",
-    } ->
+  } ->
   file {
     "/home/skltp/Pictures/eHalsa_green_white_cmyk-81.png":
     source => "/vagrant/puppet/files/users/skltp/eHalsa_green_white_cmyk-81.png",
     owner => 'skltp',
     group => 'skltp',
     mode   => 644,    # rw-r--r--    
-    } ->
+  } ->
+  file {
+    "/home/skltp/Desktop/gedit.desktop":
+    source => "/vagrant/puppet/files/users/skltp/Desktop/gedit.desktop",
+    owner => 'skltp',
+    group => 'skltp',
+    mode   => 744,    # rw-r--r--    
+  } ->
+  file {
+    "/home/skltp/Desktop/gnome-terminal.desktop":
+    source => "/vagrant/puppet/files/users/skltp/Desktop/gnome-terminal.desktop",
+    owner => 'skltp',
+    group => 'skltp',
+    mode   => 744,    # rw-r--r--    
+  } ->
+  file {
+    "/home/skltp/Desktop/gnome-nautilus-browser.desktop":
+    source => "/vagrant/puppet/files/users/skltp/Desktop/gnome-nautilus-browser.desktop",
+    owner => 'skltp',
+    group => 'skltp',
+    mode   => 744,    # rw-r--r--    
+  } ->
+  file {
+    "/home/skltp/Desktop/mozilla-firefox.desktop":
+    source => "/vagrant/puppet/files/users/skltp/Desktop/mozilla-firefox.desktop",
+    owner => 'skltp',
+    group => 'skltp',
+    mode   => 744,    # rw-r--r--    
+  } ->
+  file {
+    "/home/skltp/Desktop/soapUI.desktop":
+    source => "/vagrant/puppet/files/users/skltp/Desktop/soapUI.desktop",
+    owner => 'skltp',
+    group => 'skltp',
+    mode   => 744,    # rw-r--r--    
+  } ->
   file {
     "/etc/sysconfig/keyboard":
     source => "/vagrant/puppet/files/cent-os-config/etc/sysconfig/keyboard",
     owner => 'root',
     group => 'root',
     mode   => 644,    # rw-r--r--    
-    } ->
-    exec { "gconftool-2":
-      command => "/usr/bin/sudo -u skltp /usr/bin/gconftool-2 --load /vagrant/puppet/files/users/skltp/gconftool-2-dump.xml && touch /var/local/puppet::${title}::gconftool-2.semaphore",
-      creates => "/var/local/puppet::${title}::gconftool-2.semaphore"
-    }
+  } ->
+  exec { "gconftool-2":
+    command => "/usr/bin/sudo -u skltp /usr/bin/gconftool-2 --load /vagrant/puppet/files/users/skltp/gconftool-2-dump.xml && touch /var/local/puppet::${title}::gconftool-2.semaphore",
+    creates => "/var/local/puppet::${title}::gconftool-2.semaphore"
+  }
     
 
   include base
